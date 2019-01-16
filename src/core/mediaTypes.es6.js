ddc75efe41b4d266a9eda5ba64b8304ab4810958
Anna Pyteraf <anna.pyteraf@fortedigital.no>
2019-01-16 20:15:14 +0100

    Revert "debbuging2"
    This reverts commit c08cd9a5

--- src/core/mediaTypes.es6.js
+++ src/core/mediaTypes.es6.js
@@ -1,7 +1,5 @@
-const Media = Object.freeze({
+export default const Media = Object.freeze({
 	Desktop: Symbol("desktop"),
 	Tablet: Symbol("tablet"),
 	Mobile: Symbol("mobile")
 });
-
-export default Media;
